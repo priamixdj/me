20614f644c31ad91a6000fc6923a47e49ba109da
Mattepriagggg <132686877+priamixdj@users.noreply.github.com>
2025-06-09 13:29:02 +0200

    up

--- V2.0/assets/js/index.js
+++ V2.0/assets/js/index.js
@@ -2,3 +2,8 @@
 let hamburger = document.getElementById('hamburger_menu');
 
 let navUl = hamburger.firstElementChild;
+
+// quando abbassi la visuale della finestra lo scroll va avanti
+window.addEventListener('scroll', () => {
+    
+})
