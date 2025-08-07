ad97baaf696eea62ae4609200bf2b33da094b89b
priamixdj <132686877+priamixdj@users.noreply.github.com>
2025-08-07 06:55:48 +0200

    mail

--- V2.0/assets/js/mail.js
+++ V2.0/assets/js/mail.js
@@ -1,13 +1,17 @@
-document.getElementById('myForm').addEventListener('submit', async function(e) {
-    e.preventDefault();
-    inviaEmail(luogo); 
+document.getElementById('form').addEventListener('submit', async function(e) {
+    e.preventDefault(); 
+    inviaEmail(); 
 });
 
-
-function inviaEmail(luogo) {
+function inviaEmail() {
     var nome = document.getElementById('nome').value;
     var email = document.getElementById('email').value;
-    var messaggio = document.getElementById('messaggio').value;;
+    var messaggio = document.getElementById('messaggio').value;
+
+    if (!isValidEmail(email)) {
+        alert("Inserisci un'email valida.");
+        return;
+    }
 
     var parametri = {
         nome: nome,
@@ -26,10 +30,15 @@ function inviaEmail(luogo) {
         return;
     }
 
-    emailjs.send("service_k1y60nq","template_oko65oh", parametri)
+    emailjs.send("service_k1y60nq", "template_oko65oh", parametri)
         .then(function(response) {
             alert("Mail inviata con successo!");
         }, function(error) {
             alert("Errore durante l’invio: " + JSON.stringify(error));
         });
+}
+
+function isValidEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 }
