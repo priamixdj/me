5e45cbdfb59bf37685406d719c86c9f1a39ffe20
priamixdj <132686877+priamixdj@users.noreply.github.com>
2025-08-09 09:38:17 +0200

    modify code and alignment

--- V2.0/assets/js/mail.js
+++ V2.0/assets/js/mail.js
@@ -1,44 +1,44 @@
-document.getElementById('form').addEventListener('submit', async function(e) {
-    e.preventDefault(); 
-    inviaEmail(); 
-});
+// document.getElementById('form').addEventListener('submit', async function(e) {
+//     e.preventDefault(); 
+//     inviaEmail(); 
+// });
 
-function inviaEmail() {
-    var nome = document.getElementById('nome').value;
-    var email = document.getElementById('email').value;
-    var messaggio = document.getElementById('messaggio').value;
+// function inviaEmail() {
+//     var nome = document.getElementById('nome').value;
+//     var email = document.getElementById('email').value;
+//     var messaggio = document.getElementById('messaggio').value;
 
-    if (!isValidEmail(email)) {
-        alert("Inserisci un'email valida.");
-        return;
-    }
+//     if (!isValidEmail(email)) {
+//         alert("Inserisci un'email valida.");
+//         return;
+//     }
 
-    var parametri = {
-        nome: nome,
-        email: email,
-        messaggio: messaggio,
-    };
+//     var parametri = {
+//         nome: nome,
+//         email: email,
+//         messaggio: messaggio,
+//     };
 
-    var conferma = confirm("Sei sicuro di voler inviare i dati? " +
-        "\nNome: " + nome + 
-        ",\nEmail: " + email + 
-        ",\nMessaggio: " + messaggio
-    );
+//     var conferma = confirm("Sei sicuro di voler inviare i dati? " +
+//         "\nNome: " + nome + 
+//         ",\nEmail: " + email + 
+//         ",\nMessaggio: " + messaggio
+//     );
 
-    if (!conferma) {
-        alert("Invio annullato.");
-        return;
-    }
+//     if (!conferma) {
+//         alert("Invio annullato.");
+//         return;
+//     }
 
-    emailjs.send("service_k1y60nq", "template_oko65oh", parametri)
-        .then(function(response) {
-            alert("Mail inviata con successo!");
-        }, function(error) {
-            alert("Errore durante l’invio: " + JSON.stringify(error));
-        });
-}
+//     emailjs.send("service_k1y60nq", "template_oko65oh", parametri)
+//         .then(function(response) {
+//             alert("Mail inviata con successo!");
+//         }, function(error) {
+//             alert("Errore durante l’invio: " + JSON.stringify(error));
+//         });
+// }
 
-function isValidEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
+// function isValidEmail(email) {
+//     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     return regex.test(email);
+// }
