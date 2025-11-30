12b6c4ceabce20418eb004262417a579b4d5a462
Mattepriagggg <132686877+priamixdj@users.noreply.github.com>
2025-11-30 21:55:13 +0100

    menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,5 +1,7 @@
 menu = document.getElementById('menu')
+hamburgher_menu = document.getElementById('hamburger_menu');
 
 function toggleMenu() {
     menu.classList.toggle('open');
+    hamburgher_menu.classList.toggle('open');
 }
