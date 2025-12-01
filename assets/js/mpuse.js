d597e75628feb04abe2831d0a4e8e48329fd97ec
priamixdj <132686877+priamixdj@users.noreply.github.com>
2025-12-01 21:34:41 +0100

    mouse

--- assets/js/mpuse.js
+++ assets/js/mpuse.js
@@ -3,8 +3,8 @@ let mouse = document.getElementById('mouse');
 let mousePosition = { x: 0, y: 0 };
 
 document.addEventListener('mousemove', (event) => {
-    mousePosition.x = (event.clientX) - 345;
-    mousePosition.y = (event.clientY) - 50;
+    mousePosition.x = (event.clientX) - 40;
+    mousePosition.y = (event.clientY) - 150;
 
     mouse.style.transform = "translateX(" + mousePosition.x + "px) translateY(" + mousePosition.y + "px)";
 });
