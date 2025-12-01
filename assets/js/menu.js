79cb261354c22a577fab49c9e8eb97f418a9244a
Mattepriagggg <132686877+priamixdj@users.noreply.github.com>
2025-12-01 15:20:57 +0100

    ioij

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,15 @@
 menu = document.getElementById('menu')
 hamburgher_menu = document.getElementById('hamburger_menu');
 
+click_menu = document.querySelectorAll('nav li a');
+
+click_menu.forEach(element => {
+    element.addEventListener('click', toggleMenu);
+});
+
+
 function toggleMenu() {
     menu.classList.toggle('open');
     hamburgher_menu.classList.toggle('open');
 }
+
