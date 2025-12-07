cd762ebf15e9a8a6a86b39f29f53e4218e0e6b3b
priamixdj <132686877+priamixdj@users.noreply.github.com>
2025-12-07 14:17:58 +0100

    e

--- assets/js/mouse.js
+++ assets/js/mouse.js
@@ -1,11 +1,18 @@
-
 let mouse = document.getElementById('mouse');
 let mousePosition = { x: 0, y: 0 };
 
-document.addEventListener('mousemove', (event) => {
-    
-    mousePosition.x = (event.clientX) - 40;
-    mousePosition.y = (event.clientY) - 150;
+// Controllo se il device ha un mouse
+if (window.matchMedia("(pointer: fine)").matches) {
 
-    mouse.style.transform = "translateX(" + mousePosition.x + "px) translateY(" + mousePosition.y + "px)";
-});
+    document.addEventListener('mousemove', (event) => {
+
+        mousePosition.x = event.clientX - 40;
+        mousePosition.y = event.clientY - 150;
+
+        mouse.style.transform =
+            `translate(${mousePosition.x}px, ${mousePosition.y}px)`;
+    });
+
+} else {
+    mouse.style.display = "none";
+}
